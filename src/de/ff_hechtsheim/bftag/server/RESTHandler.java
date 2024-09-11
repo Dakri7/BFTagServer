b9b852afd5b305506172fd12e0c77734ede7ad97
Dakri7 <daniel.krimm@stud.tu-darmstadt.de>
2024-09-11 13:50:41 +0200

    Added Webserver support

--- src/de/ff_hechtsheim/bftag/server/RESTHandler.java
+++ src/de/ff_hechtsheim/bftag/server/RESTHandler.java
@@ -1,10 +1,13 @@
 package de.ff_hechtsheim.bftag.server;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,18 +25,22 @@ public class RESTHandler {
 	Logger logger = LoggerFactory.getLogger(RESTHandler.class);
 	StatusTracker tracker = new StatusTracker();
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/alarm")
 	public void alarmPost(@RequestBody AlarmObject ao) {
 		AlarmHandler alarmHandler = new AlarmHandler();
 		logger.info("Alarm received");
+		logger.info(ao.toDisplayString());
 		alarmHandler.alarmReceived(ao);
 	}
-
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/status")
 	private @ResponseBody String getStatus(String vehicle) {
 		return tracker.getFullStatusForVehicle(vehicle);
 	}
-
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping("/status")
 	public String putStatus(@RequestParam String vehicle, @RequestParam String status) {
 		tracker.setStatusForVehicle(vehicle, status);
@@ -41,31 +48,49 @@ public class RESTHandler {
 		return tracker.getFullStatusForVehicle(vehicle);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/vehicles")
 	public String[] getVehicleList() {
 		return new String[]{"LF 8/12", "LF 20/16"};
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/groups")
 	public String[] getGroups() {
 		return new String[]{"Gruppe 1", "Gruppe 2", "Gruppe 3"};
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/street-names")
+	public String[] getStreetNames() throws IOException{
+		return ResourceManager.getStreetNames();
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/aao")
+	public String[] getAAO(@RequestParam String category) throws IOException{
+		return ResourceManager.getAAO(category);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/alarms")
 	public @ResponseBody Map<String, AlarmObject> getAlarms(){
 		return AlarmSerializationHandler.getAlarms();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping("/alarm")
 	public void addAlarm(@RequestParam String name, @RequestBody AlarmObject ao){
 		AlarmSerializationHandler.saveAlarm(name, ao);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/alarm")
 	public void deleteAlarm(@RequestParam String name){
 		AlarmSerializationHandler.deleteAlarm(name);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/announcement")
 	public void triggerAnnouncement(@RequestBody String text){
 		AlarmTTS alarmTTS = new AlarmTTS();
---
+++ src/de/ff_hechtsheim/bftag/server/ResourceManager.java
@@ -0,0 +1,23 @@
+package de.ff_hechtsheim.bftag.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ResourceManager {
+	
+	private static String streetNamesFile = "./StreetNames.txt";
+	private static String aaoFolder = "./AAO/";
+	
+	public static String[] getStreetNames() throws IOException {
+		String allFiles = Files.readString(Path.of(streetNamesFile), StandardCharsets.UTF_8);
+		return allFiles.split("\r?\n");
+	}
+	
+	public static String[] getAAO(String category) throws IOException {
+		String allFiles = Files.readString(Path.of(aaoFolder+category+".txt"), StandardCharsets.UTF_8);
+		return allFiles.split("\r?\n");
+	}
+}
