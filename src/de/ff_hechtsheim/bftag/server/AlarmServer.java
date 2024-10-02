35ffc5bcc3746c10d6809f862a46c26d568abd41
dakri7 <danikrimm99@gmail.com>
2024-10-02 14:13:26 +0200

    I dont want to talk about it

--- src/de/ff_hechtsheim/bftag/server/AlarmServer.java
+++ src/de/ff_hechtsheim/bftag/server/AlarmServer.java
@@ -8,6 +8,7 @@ import java.net.SocketException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 
